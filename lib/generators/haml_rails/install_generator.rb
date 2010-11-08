e6cb88932c305337bb34e8d8efbc4eac7347a5a1
Rafael Felix <felix.rafael@gmail.com>
2010-11-07 23:47:06 -0200

    Added comments for installing

--- lib/generators/haml_rails/install_generator.rb
+++ lib/generators/haml_rails/install_generator.rb
@@ -5,18 +5,30 @@ module HamlRails
       source_root File.expand_path('../templates', __FILE__)
       
       def create_default_folder_structure
+        puts "Creating folders..."
         @haml_dir = "lib/templates/haml"
         Dir.mkdir("lib/templates") unless File.exists? "lib/templates"
         Dir.mkdir(@haml_dir) unless File.exists? @haml_dir
       end
       def copy_controller_template
+        puts "Coping controllers templates"
         FileUtils.cp_r File.expand_path('../templates/controller', __FILE__), @haml_dir
       end
       def copy_mailer_template
+        puts "Coping mailer templates"
         FileUtils.cp_r File.expand_path('../templates/mailer', __FILE__), @haml_dir
       end
-      def copy_scaffold_template        
+      def copy_scaffold_template
+        puts "Coping scaffold templates"
         FileUtils.cp_r File.expand_path('../templates/scaffold', __FILE__), @haml_dir
+      end
+      def print_usage
+        puts "The Rails templates for haml was configured"
+        puts "Don't forget to add the haml to your Gemfile"
+        puts "Change your config/application.rb and add"
+        puts "  config.generators do |g|               "
+        puts "  g.template_engine :haml                "
+        puts "  end                                    "
       end
     end
   end
